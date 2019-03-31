0abae3a5701fbc7fb5c73eb96e73e58ab109ade0
David Harabagiu <david.harabagiu@telenav.com>
2019-03-31 16:10:55 +0300

    Upload api

--- helios-server/server/server.js
+++ helios-server/server/server.js
@@ -1,11 +1,5 @@
 var app = require('express')();
-var bodyParser = require('body-parser');
 var urls = require('./urls');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
 app.get('/', (request, response) => {
     response.send('Welcome to Helios!');
