597e754f3c7e9a60a9cf9707599530760a17237a
David Harabagiu <harabagiudavid@gmail.com>
2019-05-20 16:43:44 +0300

    Enabled HTTPS

--- helios-server/server/server.js
+++ helios-server/server/server.js
@@ -1,5 +1,14 @@
 var app = require('express')();
 var urls = require('./urls');
+var fs = require('fs');
+var https = require('https');
+
+var privateKey = fs.readFileSync('../../localhost.key', 'utf8');
+var certificate = fs.readFileSync('../../localhost.crt', 'utf8');
+var credentials = {
+    key: privateKey,
+    cert: certificate
+};
 
 app.get('/', (request, response) => {
     response.send('Welcome to Helios!');
@@ -7,8 +16,10 @@ app.get('/', (request, response) => {
 
 urls.register(app);
 
-var server = app.listen(8000, function() {
-    var host = server.address().address;
-    var port = server.address().port;
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(8443, function() {
+    var host = httpsServer.address().address;
+    var port = httpsServer.address().port;
     console.log(`Listening on ${host}:${port}...`);
 });
